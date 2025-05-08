1dcc249de14cdf244a7876d83d1748a1c4e97a94
Brigham Dean <Briggs-dean@outlook.com>
2025-05-07 17:24:18 -0800

    tst

---
+++ readmee.js
@@ -0,0 +1,12 @@
+class House {
+    constructor(color) {
+      this.color = color;
+    }
+    getFurniture() {
+      return 'sofa';
+    }
+  }
+
+  const houseObject = new House('red');
+const houseObject2 = new House('blue');
+console.log(houseObject2.getFurniture());
